6ac984c0899ac27bf3be6bdaca902fe2f9cabd5e
ngoductuanlhp <ngoductuanlhp@gmail.com>
2023-08-26 10:43:37 -0400

    # update pretrain and dyco

--- isbnet/model/isbnet.py
+++ isbnet/model/isbnet.py
@@ -179,8 +179,17 @@ class ISBNet(nn.Module):
         ]
         self.add_module("mask_tower", nn.Sequential(*mask_tower))
 
-        weight_nums = [(self.mask_dim_out + 3 + 3) * self.mask_dim_out, self.mask_dim_out * (self.mask_dim_out//2), (self.mask_dim_out//2) * 1]
-        bias_nums = [self.mask_dim_out, (self.mask_dim_out//2), 1]
+        if hasattr(self.instance_head_cfg, 'num_dyco_layer'):
+            self.num_dyco_layer = self.instance_head_cfg.num_dyco_layer
+        else:
+            self.num_dyco_layer = 3
+
+        if self.num_dyco_layer == 3:
+            weight_nums = [(self.mask_dim_out + 3 + 3) * self.mask_dim_out, self.mask_dim_out * (self.mask_dim_out//2), (self.mask_dim_out//2) * 1]
+            bias_nums = [self.mask_dim_out, (self.mask_dim_out//2), 1]
+        else:
+            weight_nums = [(self.mask_dim_out + 3 + 3) * (self.mask_dim_out//2), (self.mask_dim_out//2) * 1]
+            bias_nums = [(self.mask_dim_out//2), 1]
 
         self.weight_nums = weight_nums
         self.bias_nums = bias_nums
@@ -788,12 +797,18 @@ class ISBNet(nn.Module):
         weight_splits = params_splits[:num_layers]
         bias_splits = params_splits[num_layers:]
 
-        weight_splits[0] = weight_splits[0].reshape(num_instances, out_channels + 6, out_channels)
-        bias_splits[0] = bias_splits[0].reshape(num_instances, out_channels)
-        weight_splits[1] = weight_splits[1].reshape(num_instances, out_channels, out_channels // 2)
-        bias_splits[1] = bias_splits[1].reshape(num_instances, out_channels // 2)
-        weight_splits[2] = weight_splits[2].reshape(num_instances, out_channels // 2, 1)
-        bias_splits[2] = bias_splits[2].reshape(num_instances, 1)
+        if self.num_dyco_layer == 3:
+            weight_splits[0] = weight_splits[0].reshape(num_instances, out_channels + 6, out_channels)
+            bias_splits[0] = bias_splits[0].reshape(num_instances, out_channels)
+            weight_splits[1] = weight_splits[1].reshape(num_instances, out_channels, out_channels // 2)
+            bias_splits[1] = bias_splits[1].reshape(num_instances, out_channels // 2)
+            weight_splits[2] = weight_splits[2].reshape(num_instances, out_channels // 2, 1)
+            bias_splits[2] = bias_splits[2].reshape(num_instances, 1)
+        else:
+            weight_splits[0] = weight_splits[0].reshape(num_instances, out_channels + 6, out_channels // 2)
+            bias_splits[0] = bias_splits[0].reshape(num_instances, out_channels // 2)
+            weight_splits[1] = weight_splits[1].reshape(num_instances, out_channels // 2, 1)
+            bias_splits[1] = bias_splits[1].reshape(num_instances, 1)
 
         return weight_splits, bias_splits  # LIST OF [n_queries, C_in, C_out]
 
